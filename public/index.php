eeb394b757b4c4af4314799b1a9ad41c9d8dd06b
Sata Lesmana <lesmanasata@gmail.com>
2021-10-22 19:27:26 +0700

    Model Obal

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 // Path to the front controller (this file)
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
-define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+//define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 /*
  *---------------------------------------------------------------
